7772da3e2e84d12a6e0095f68fdc563b451430f5
Josh Beam <joshua.a.beam@gmail.com>
2015-09-21 21:07:26 -0700

    updates example to reflect readme

--- example.js
+++ example.js
@@ -8,14 +8,14 @@ lua.init(client);
  * This example requires that there exists an "ids" list of `1, 2, 3`
  * and 3 hashmaps --
  *
- * thing:1
- * thing:2
- * thing:3
+ * things:1
+ * things:2
+ * things:3
  */
 
-lua.join(['ids', 'thing', 2], function(err, res) {
+lua.join(['ids', 'things', 2], function(err, res) {
   /**
-   * This will return the hashes thing:3, and thing:2
+   * This will return the hashes things:3, and things:2
    */
   console.log(res);
   client.quit();
